7ec487a72a8eeac7575b17cc7c0db55f195a0b9a
oskalis <oskalis@yandex.ru>
2025-10-17 23:59:41 +0300

    korekciya_*.ipynb

--- project_fastapi/main.py
+++ project_fastapi/main.py
@@ -1,15 +1,21 @@
+import logging
+import datetime as dt
 import pandas as pd
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.datastructures import UploadFile
+from predicting_attack import RiskPrediction
 
-from predicting_attack import predict_the_risk
-
+logging.basicConfig(
+    filename='main.log',
+    level=logging.INFO
+)
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
+new_file = RiskPrediction()
 
-FILE_TEMP: str = ''
+logging.info(f'[{dt.datetime.now()}] Старт проекта !!!')
 
 @app.get('/')
 def main(request: Request):
@@ -19,22 +25,23 @@ def main(request: Request):
 @app.get('/result', response_class=HTMLResponse)
 def main(request: Request):
     """Страница получения результата"""
-    return templates.TemplateResponse('result.html', {'request': request, 'file_name': FILE_TEMP})
+    return templates.TemplateResponse('result.html', {'request': request, 'file_name': new_file.file_name})
 
 @app.post('/submits')
 def submits(file: UploadFile):
     """Загруска фаила с данными и обработка его к конечному результату"""
-    global  FILE_TEMP
-    FILE_TEMP = file.filename
-    df = pd.read_csv(file.file)
-    predict_the_risk(file.filename, df)
+    new_file.add(file.filename, pd.read_csv(file.file))
+    new_file.predict_the_risk()
     return {'url': '/result'}
 
 @app.get("/download")
-def download_file():
+def download_file(request: Request):
     """Эндпоинт на скачивание готового файла"""
+    if not new_file.get_file():
+        text = 'Фаил не создан попробуйте ещё раз !!!'
+        return templates.TemplateResponse('result.html', {'request': request, 'file_name': text})
     return FileResponse(
-        path=FILE_TEMP,
-        filename='предсказание.csv',
-        media_type='multipart/form-data'
-    )
+            path=new_file.get_file(),
+            filename=new_file.file_name,
+            media_type='multipart/form-data'
+        )
--- project_fastapi/predicting_attack.py
+++ project_fastapi/predicting_attack.py
@@ -2,12 +2,36 @@
 Обработка файла и дополнить его предсказанием по риску приступа
 """
 
+import os
 import csv
+import logging
+import datetime as dt
 from joblib import load
 
-def predict_the_risk(file_name: str, file: object):
-    """Получаем фаил и делеем предсказание на данных и сохраняем фаил"""
-    best_model = load('best_model.joblib')
-    # Подготовка данных для предсказания
-    file['y_pred'] = best_model.decision_function(file)
-    file.to_csv(file_name, index=False)
+class RiskPrediction:
+
+    file_name: str = ''
+    temp_file = 'temp_file'
+    data: object = None
+    model = 'best_model.joblib'
+
+    def add(self, file_name: str, data: object):
+        """Добавление название файла и сами данные"""
+        self.file_name = file_name
+        self.data = data
+
+    def predict_the_risk(self):
+        """Делаю предсказание"""
+        if os.path.isfile(self.model):
+            best_model = load('best_model.joblib')
+            #self.data['y_pred'] = best_model.decision_function(self.data)
+            self.data.to_csv(self.temp_file, index=False)
+        else:
+            logging.error(f'[{dt.datetime.now()}] Фаил обученой модели не найден.')
+
+    def get_file(self):
+        """Получить фаил"""
+        if not os.path.isfile(self.temp_file):
+            logging.error(f'[{dt.datetime.now()}] Фаил предсказание не найден.')
+            return False
+        return self.temp_file
